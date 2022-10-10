96cb2fc73f3d8687099844f748d39bc96d5ed168
rahmanabdul2k <114925639+rahmanabdul2k@users.noreply.github.com>
2022-10-10 12:08:00 +0530

    Update hackerrank.java

--- hackerrank.java
+++ hackerrank.java
@@ -109,3 +109,52 @@ public class Solution {
 
     }
 }
+
+           6 java loop | 
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i = 1; i <=10; i++)
+        {
+            System.out.println(N + " x "+i+" = "+i*N);
+        }
+    }
+}
+
+
+         7 java loop ||
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+     public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int calc = a;
+            for(int j=0;j<n;j++)
+            {
+                calc+=(Math.pow(2,j)*b);
+                System.out.print(calc+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
