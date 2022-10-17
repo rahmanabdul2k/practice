3112ab7a953020ac64c8174c54928dc5caea48ee
rahmanabdul2k <114925639+rahmanabdul2k@users.noreply.github.com>
2022-10-17 12:54:51 +0530

    Update hackerrank.java

--- hackerrank.java
+++ hackerrank.java
@@ -468,3 +468,30 @@ class Solution {
         }
     }
 }
+
+
+                  16 Java Strings Introduction
+		  
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length() + B.length());
+    if(A.compareTo(B) > 0){
+        System.out.println("Yes");
+    } else {
+        System.out.println("No");
+    }
+
+    A = (A.substring(0,1)).toUpperCase() + A.substring(1);       
+    B = (B.substring(0,1)).toUpperCase() + B.substring(1);
+    System.out.println(A + " " + B);
+    }
+}		  
+		  
